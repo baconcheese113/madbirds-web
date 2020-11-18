07646463936634e1713344168ab560581e2f27d9
Joseph Yannessa <baconcheese113@hotmail.com>
2020-11-17 23:55:11 -0700

    First pass of server code added and model created

---
+++ server/src/schema.ts
@@ -0,0 +1,83 @@
+import { nexusPrisma } from 'nexus-plugin-prisma'
+import { makeSchema, objectType, queryType } from '@nexus/schema'
+import { join } from 'path'
+
+
+const schema = makeSchema({
+  types: [
+    queryType({
+      definition(t) {
+        t.crud.scene({})
+        t.crud.scenes({})
+      }
+    }),
+    objectType({
+      name: 'Scene',
+      definition(t) {
+        t.model.id()
+        t.model.levelNumber()
+        t.model.crates()
+        t.model.explosiveCrates()
+        t.model.enemies()
+        t.model.platforms()
+      }
+    }),
+    objectType({
+      name: 'Crate',
+      definition(t) {
+        t.model.id()
+        t.model.x()
+        t.model.y()
+        t.model.rotation()
+        t.model.Scene()
+      }
+    }),
+    objectType({
+      name: 'ExplosiveCrate',
+      definition(t) {
+        t.model.id()
+        t.model.x()
+        t.model.y()
+        t.model.rotation()
+        t.model.explosionRadius()
+        t.model.explosionForce()
+        t.model.hitSensitivity()
+        t.model.chainReactionRadius()
+        t.model.onlyPlayerCanTrigger()
+        t.model.Scene()
+      }
+    }),
+    objectType({
+      name: 'Enemy',
+      definition(t) {
+        t.model.x()
+        t.model.y()
+        t.model.rotation()
+        t.model.Scene()
+      }
+    }),
+    objectType({
+      name: 'Platform',
+      definition(t) {
+        t.model.x()
+        t.model.y()
+        t.model.rotation()
+        t.model.useRotator()
+        t.model.degsPerSec()
+        t.model.useBouncer()
+        t.model.bounceMultiplier()
+        t.model.onlyBounceFront()
+        t.model.Scene()
+      }
+    })
+  ],
+  outputs: {
+    typegen: join(__dirname, '..', 'nexus-typegen.ts'),
+    schema: join(__dirname, '..', 'schema.graphql'),
+  },
+  plugins: [nexusPrisma({
+    experimentalCRUD: true,
+  })],
+})
+
+export { schema }
